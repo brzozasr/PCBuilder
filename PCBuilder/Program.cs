368c165a340b2063dab6ecf0ddc68f0379a82cac
Sławomir Brzozowski <brzozasr@interia.eu>
2021-03-11 08:11:16 +0100

    Init commit.

---
+++ PCBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PCBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
